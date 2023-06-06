c1970428c98e5d4500dbc6dd7f1721a1a4bb9eed
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-06-06 16:48:02 +0200

    [JS] Added Logic Palindrome

--- js/script.js
+++ js/script.js
@@ -1,1 +1,41 @@
 console.log("JSOK");
+
+// Recupero gli elementi
+
+const palindromeElement = document.querySelector("#pali input");
+const palindromeBtn = document.querySelector("#pali button");
+const palindromeResult = document.getElementById("palindrome-result");
+
+// Creiamo una funzione
+
+function isPalindrome(word) {
+  let newWord = "";
+
+  for (let i = word.length - 1; i >= 0; i--) {
+    newWord += word.at(i);
+  }
+
+  return word === newWord;
+}
+
+// Event Listener
+
+palindromeBtn.addEventListener("click", function () {
+  const userWord = palindromeElement.value.trim();
+
+  //   Stampiamo in pagina "Non è Palindrome"
+
+  if (!userWord) return;
+
+  let result = "<strong>NON E'</strong>";
+
+  //   Stampiamo in pagina "E' Palindrome"
+
+  const palindrome = isPalindrome(userWord);
+
+  if (palindrome) result = "<strong>E'</strong>";
+
+  const message = `'${userWord}' ${result} Palindrome!`;
+
+  palindromeResult.innerHTML = message;
+});
